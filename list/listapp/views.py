9b550b611389b17f425e8ae84cd2c634527a215e
Abhishek Mangalur <121670647+Abhishek-Mangalur@users.noreply.github.com>
2024-07-21 09:14:41 +0530

    Delete list directory

--- list/listapp/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-
-def showlist(request):
-    students = ["Tony", "Mony", "Sony", "Bob"]
-    fruits = ["Mango", "Banana", "Apple", "Jackfruit"]
-    return render(request, 'index.html', {"fruits": fruits, "students": students})
