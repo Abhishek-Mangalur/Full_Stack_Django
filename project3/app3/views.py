901639a4cfb2c0f908e873ae75ca607e49ed7a3b
Abhishek <abhishekm.21.beis@acharya.ac.in>
2024-08-15 10:38:08 +0530

    Current Date_time

--- project3/app3/views.py
+++ project3/app3/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
 import datetime
+
 def current_datetime(request):
     now=datetime.datetime.now()
     html="<html><body><h1>It is now %s </h1></body></html>" % now
